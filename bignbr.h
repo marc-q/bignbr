04f538ec3d51eee48b01c8a554a87513e5fc813a
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2016-08-06 19:05:23 +0200

    Update bignbr.h
    Reorder the functions.

--- bignbr.h
+++ bignbr.h
@@ -7,6 +7,8 @@
 /* SIGN */
 #define BIGNBR_SIGN 0
 
+#define BIGNBR_GETNBR(a) ((a) == (BIGNBR_EON) ? (0) : (a))
+
 struct _bignbr
 {
 	unsigned int len;
@@ -16,15 +18,15 @@ struct _bignbr
 typedef struct _bignbr bignbr;
 
 /* Core */
-void bignbr_fill (bignbr*, unsigned char*);
 void bignbr_init (bignbr*, unsigned int, unsigned char*);
 void bignbr_free (bignbr*);
-void bignbr_print (bignbr*);
 void bignbr_cpy (bignbr*, bignbr*);
+void bignbr_fill (bignbr*, unsigned char*);
+void bignbr_print (bignbr*);
+unsigned int bignbr_get_eon_pos (bignbr*);
 
+/* Check */
 bool bignbr_cmp_str (bignbr*, unsigned char*);
-
-/* Checks */
 bool bignbr_is_null (bignbr*);
 
 void bignbr_set_negative (bignbr*, bool);
